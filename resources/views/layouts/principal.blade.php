5753e12c440897d4488ab352fb2736bb7acdbfa0
IsaCG14 <isabelcgraterolp@gmail.com>
2024-01-24 10:57:52 -0400

    Añadida cdn

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     {!! Html::style("css/styles.css") !!}
     {!! Html::style("libraries/bootstrap.min.css") !!}
     <style>
@@ -39,6 +40,8 @@
             </div>
         </div>
     </nav>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="{{ asset('libraries/jquery-3.7.1.min.js') }}"></script>
     <script src="{{ asset('libraries/bootstrap.bundle.min.js') }}"></script>
     @yield('content') 
